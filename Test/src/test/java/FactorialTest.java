9bca24a0d1464a233e95e53a7711d32d75fab164
Uzer <dariapodroikina@gmail.com>
2024-07-12 20:42:01 +0300

    Выполненное задание 10 testng

---
+++ Test/src/test/java/FactorialTest.java
@@ -0,0 +1,22 @@
+import Lesson10.Factorial;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class FactorialTest {
+    private Factorial factorial;
+    @BeforeMethod
+    public void setUp() {
+        factorial = new Factorial();
+    }
+    @Test
+    public void printFactorialPositiveTest() {
+        long result = factorial.printFactorial(3);
+        assertEquals(6, result);
+    }
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void printFactorialNegativeTest() {
+        factorial.printFactorial(-1);
+    }
+}
