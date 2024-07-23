0eaff93a5a51c7d3dbce93329a0d6399ec53aacf
Uzer <dariapodroikina@gmail.com>
2024-07-23 22:04:09 +0300

    Выполненное задание 14

--- src/test/java/Lesson_13/MTSTest.java
+++ src/test/java/Lesson_13/MTSTest.java
@@ -1,58 +1,77 @@
 package Lesson_13;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 
 public class MTSTest {
-
     private WebDriver driver;
 
-    @BeforeTest
-    private void beforeTest() {
+    @BeforeClass
+    void setup() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.mts.by/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        WebElement button = driver.findElement(By.xpath("//*[@id='cookie-agree']"));
-        button.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement click = driver.findElement(By.xpath("//button[contains(@class, 'cookie__cancel')]"));
+        click.click();
     }
 
-    @Test(priority = 1)
-    public void checkOnlineReplenishment() {
-        Assert.assertEquals(driver.findElement(By.cssSelector("div.pay__wrapper h2")).getText(),
-                "Онлайн пополнение\nбез комиссии");
+
+    @Test
+    void titleTest() {
+        WebElement title = driver.findElement(By.xpath("//h2[text()='Онлайн пополнение ']"));
+        Assert.assertEquals(title.getText(), "Онлайн пополнение без комиссии",
+                "Different actual result. Click the link below for details");
     }
 
-    @Test(priority = 2)
-    public void checkLogoPaymentSystems() {
-        WebElement logo = driver.findElement(By.xpath("//div[@class='pay__partners']//img"));
-        Actions action = new Actions(driver);
-        action.moveToElement(logo).perform();
-        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='pay__partners']//img")), logo);
+    @Test
+    void logoTest() {
+        WebElement visa = driver.findElement
+                (By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[2]/ul/li[1]/img"));
+        Assert.assertTrue(visa.isDisplayed());
+
+        WebElement verifiedByVisa = driver.findElement
+                (By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[2]/ul/li[2]/img"));
+        Assert.assertTrue(verifiedByVisa.isDisplayed());
+
+        WebElement masterCard = driver.findElement
+                (By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[2]/ul/li[3]/img"));
+        Assert.assertTrue(masterCard.isDisplayed());
+
     }
 
-    @Test(priority = 3)
-    public void checkLinkWork() {
-        WebElement link = driver.findElement(By.xpath("//a[contains(@href,'/help/poryadok-oplaty')]"));
-        link.click();
-        driver.get("https://www.mts.by/");
+    @Test
+    void linkTest() {
+        WebElement readMore = driver.findElement(By.linkText("Подробнее о сервисе"));
+        Assert.assertTrue(readMore.isEnabled());
+        readMore.click();
+        driver.navigate().back();
     }
 
-    @Test(priority = 4)
-    public void checkButtonContinue() {
-        WebElement input1 = driver.findElement(By.xpath("//input[@placeholder='Номер телефона']"));
-        input1.sendKeys("297777777");
-        WebElement input2 = driver.findElement(By.xpath("//input[@placeholder='Сумма']"));
-        input2.sendKeys("1");
-        WebElement input3 = driver.findElement(By.xpath(
-                "//form[@class='pay-form opened']//button[@class='button button__default ']"));
-        input3.click();
+    @Test
+    void topUpFormTest() {
+        WebElement number = driver.findElement(By.xpath("//input[@id='connection-phone']"));
+        number.sendKeys("297777777");
+
+        WebElement money = driver.findElement(By.xpath("//input[@id='connection-sum']"));
+        money.sendKeys("10");
+
+        WebElement continueButton = driver.findElement(By.xpath("//*[@id='pay-connection']/button"));
+        Assert.assertTrue(continueButton.isEnabled());
     }
 
+    @AfterClass
+    void shutDown() {
+        driver.quit();
+    }
 }
